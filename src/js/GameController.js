1c4b8b86b3844ffc74e529566aade0770be6f61d
Shulaev Ivan <mail@ntdservice.ru>
2022-11-14 14:03:13 +0300

    initial

---
+++ src/js/GameController.js
@@ -0,0 +1,27 @@
+import themes from './themes.js';
+
+export default class GameController {
+  constructor(gamePlay, stateService) {
+    this.gamePlay = gamePlay;
+    this.stateService = stateService;
+    this.themes = this.themes
+  }
+
+  init() {
+    this.gamePlay.drawUi(themes.prairie);
+    // TODO: add event listeners to gamePlay events
+    // TODO: load saved stated from stateService
+  }
+
+  onCellClick(index) {
+    // TODO: react to click
+  }
+
+  onCellEnter(index) {
+    // TODO: react to mouse enter
+  }
+
+  onCellLeave(index) {
+    // TODO: react to mouse leave
+  }
+}
